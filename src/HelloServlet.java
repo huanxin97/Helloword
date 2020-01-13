471d9bf41dc6495c71a03209b52a9b32e556004f
Shinelon <1921937233@qq.com>
2020-01-13 20:40:33 +0800

    test

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println("<h1>Hello Servlet111!</h1>");
             response.getWriter().println(new Date());
         } catch (IOException e) {
             // TODO Auto-generated catch block
